ea789305075670dee736494e2c65b1f04a915200
Mario Garcia <learningmariogg@gmail.com>
2022-12-13 19:03:43 +0100

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
 import { NgModule } from '@angular/core';
+
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+import { ContadorModule } from './contador/contador.module';
+
+import { HeroeModule } from './heroes/heroes.module';
+ 
+ 
+
 @NgModule({
+
   declarations: [
-    AppComponent
+    
+    AppComponent,
+
+    
   ],
+  
   imports: [
-    BrowserModule
+    BrowserModule,
+
+    HeroeModule,
+
+    ContadorModule
   ],
+
   providers: [],
   bootstrap: [AppComponent]
+
 })
+
+
 export class AppModule { }
